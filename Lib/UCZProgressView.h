e68c7e40624a75f623037a4044f04bd0b2d647d6
Simon Matsepura <semen.matsepura@yandex.ru>
2017-07-25 18:48:50 +0300

    Update UCZProgressView.h

--- Lib/UCZProgressView.h
+++ Lib/UCZProgressView.h
@@ -8,25 +8,23 @@
 
 #import <UIKit/UIKit.h>
 
-IB_DESIGNABLE
-
 @interface UCZProgressView : UIView
 
-@property (nonatomic) IBInspectable BOOL indeterminate;
-@property (nonatomic) IBInspectable CGFloat progress;
-@property (nonatomic) IBInspectable BOOL showsText; // UI_APPEARANCE_SELECTOR;
+@property (nonatomic) BOOL indeterminate;
+@property (nonatomic) CGFloat progress;
+@property (nonatomic) BOOL showsText; // UI_APPEARANCE_SELECTOR;
 
-@property (nonatomic) IBInspectable CGFloat lineWidth; // UI_APPEARANCE_SELECTOR;
-@property (nonatomic) IBInspectable CGFloat radius; // UI_APPEARANCE_SELECTOR;
-@property (nonatomic) IBInspectable UIColor *tintColor; // UI_APPEARANCE_SELECTOR;
+@property (nonatomic) CGFloat lineWidth; // UI_APPEARANCE_SELECTOR;
+@property (nonatomic) CGFloat radius; // UI_APPEARANCE_SELECTOR;
+@property (nonatomic) UIColor *tintColor; // UI_APPEARANCE_SELECTOR;
 @property (nonatomic) UIView *backgroundView; // UI_APPEARANCE_SELECTOR;
 
 @property (nonatomic, readonly) UILabel *textLabel; // UI_APPEARANCE_SELECTOR;
-@property (nonatomic) IBInspectable UIColor *textColor; // UI_APPEARANCE_SELECTOR;
-@property (nonatomic) IBInspectable CGFloat textSize; // UI_APPEARANCE_SELECTOR;
+@property (nonatomic) UIColor *textColor; // UI_APPEARANCE_SELECTOR;
+@property (nonatomic) CGFloat textSize; // UI_APPEARANCE_SELECTOR;
 
 @property (nonatomic) UIBlurEffect *blurEffect NS_AVAILABLE_IOS(8_0); // UI_APPEARANCE_SELECTOR;
-@property (nonatomic) IBInspectable BOOL usesVibrancyEffect; // UI_APPEARANCE_SELECTOR;
+@property (nonatomic) BOOL usesVibrancyEffect; // UI_APPEARANCE_SELECTOR;
 
 @property (nonatomic, copy) void(^animationDidStopBlock)();
 
